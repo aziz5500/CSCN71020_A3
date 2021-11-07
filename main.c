f671c48fde8fa074c63eae0e6f1d1a8ee1816bbc
aziz5500 <90855861+aziz5500@users.noreply.github.com>
2021-11-06 22:54:38 -0400

    Added a function RockPaperScissorsGame

--- main.c
+++ main.c
@@ -5,6 +5,13 @@ int main(void)
 
 char* RockPaperScissorsGame(char* player1, char* player2)
 {
- 
-
+	
+	if (player1 == "Rock" && player2 == "Paper" || player1 == "Scissors" && player2 == "Rock" || player1 == "Paper" && player2 == "Scissors")
+		return "Player2";
+	else if (player1 == "Paper" && player2 == "Rock" || player1 == "Rock" && player2 == "Scissors" || player1 == "Scissors" && player2 == "Paper")
+		return "Player1";
+	else if (player1 == "Rock" && player2 == "Rock" || player1 == "Scissors" && player2 == "Scissors" || player1 == "Paper" && player2 == "Paper")
+		return "Draw";
+	else
+		return "Invalid";
 }
