8ef20cd25387d92867de0bcbff9398eb4d4f09f6
aziz5500 <90855861+aziz5500@users.noreply.github.com>
2021-11-07 09:39:08 -0500

    Did the formatting and Naming the tests as advised Added comments for better understanding.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int main(void) 
 {
 	return 0;
@@ -7,11 +8,11 @@ char* RockPaperScissorsGame(char* player1, char* player2)
 {
 	
 	if (player1 == "Rock" && player2 == "Paper" || player1 == "Scissors" && player2 == "Rock" || player1 == "Paper" && player2 == "Scissors")
-		return "Player2";
+		return "Player2"; //returning the value to Player2
 	else if (player1 == "Paper" && player2 == "Rock" || player1 == "Rock" && player2 == "Scissors" || player1 == "Scissors" && player2 == "Paper")
-		return "Player1";
+		return "Player1"; //returning the value to Player1
 	else if (player1 == "Rock" && player2 == "Rock" || player1 == "Scissors" && player2 == "Scissors" || player1 == "Paper" && player2 == "Paper")
-		return "Draw";
+		return "Draw";  //returning the value to Draw
 	else
 		return "Invalid";
 }
